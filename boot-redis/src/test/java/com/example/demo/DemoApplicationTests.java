2144cb379c5a45e3c100d71509f5b3b3122ab4a0
hantsy <hantsy@gmail.com>
2025-08-02 22:20:54 +0800

    chore: clean codes

---
+++ boot-redis/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+@Slf4j
+class DemoApplicationTests {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    public void testProductRepository() {
+        var product = productRepository.save(new Product(null, "test", BigDecimal.ONE));
+        assertThat(product).isNotNull();
+        assertThat(product.id()).isNotNull();
+
+        productRepository.findById(product.id()).ifPresent(
+                p -> {
+                    log.debug("found product by id: {}", p);
+                    assertThat(p.name()).isEqualTo("test");
+                }
+        );
+    }
+
+}
