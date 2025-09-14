f4ac8ec932879cfbfd0945cb2e67a2db9b1a6dcb
hantsy <hantsy@gmail.com>
2025-09-14 21:52:56 +0800

    chore: add Hibernate Jakarta Data example

--- hibernate/src/main/java/com/example/demo/Application.java
+++ hibernate/src/main/java/com/example/demo/Application.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import com.example.demo.repository.jpa.PostRepository;
+import com.example.demo.repository.PostRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
