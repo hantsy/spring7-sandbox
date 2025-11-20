aa0e1d89172901da1603fc65914b0eea4de5c8b5
hantsy <hantsy@gmail.com>
2025-11-20 16:30:18 +0800

    chore: clean codes

--- mvc-freemarker/src/main/java/com/example/demo/web/WebConfig.java
+++ mvc-freemarker/src/main/java/com/example/demo/web/WebConfig.java
@@ -38,7 +38,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(HttpMessageConverters.ServerBuilder builder) {
         var httpMessageConverter = new JacksonJsonHttpMessageConverter(jsonMapper);
-        builder.jsonMessageConverter(httpMessageConverter);
+        builder.addCustomConverter(httpMessageConverter);
     }
 
     @Override
