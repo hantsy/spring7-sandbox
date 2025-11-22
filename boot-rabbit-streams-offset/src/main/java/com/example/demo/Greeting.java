a1e759e1fe7e020625982e156ab78b3c9062f8a1
hantsy <hantsy@gmail.com>
2025-11-22 15:24:30 +0800

    chore: clean codes

--- boot-rabbit-streams-offset/src/main/java/com/example/demo/Greeting.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import java.time.Instant;
-
-record Greeting(String message, Instant sentAt) {
-    public static Greeting of(String message) {
-        return new Greeting(message, Instant.now());
-    }
-}
