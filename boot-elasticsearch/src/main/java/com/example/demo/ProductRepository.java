b9e537202dd2077f0bb53681aded53bc8c5862f7
hantsy <hantsy@gmail.com>
2025-12-06 13:21:38 +0800

    chore: clean elasticsearch example

---
+++ boot-elasticsearch/src/main/java/com/example/demo/ProductRepository.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.GetResponse;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ProductRepository {
+
+    private final ElasticsearchClient client;
+
+    @SneakyThrows
+    Product save(Product product) {
+        var id = product.id() != null ? product.id() : UUID.randomUUID().toString();
+        log.debug("Saving product with id={}", id);
+        IndexResponse response = client.index(builder -> builder.id(id).index("products").document(product));
+
+        var savedID = response.id();
+        log.debug("Saved product with id={}", savedID);
+        return new Product(savedID, product.name(), product.price());
+    }
+
+    @SneakyThrows
+    Optional<Product> findById(String id) {
+        GetResponse<Product> response = this.client.get(builder -> builder.id(id).index("products"), Product.class);
+
+        if (response.found()) {
+            return Optional.of(response.source());
+        }
+
+        return Optional.empty();
+    }
+}
---
+++ boot-elasticsearch/src/test/java/com/example/demo/TestcontainersConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    ElasticsearchContainer elasticsearchContainer() {
+        return new ElasticsearchContainer(DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:7.17.10"));
+    }
+
+}
